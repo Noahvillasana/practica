c97e2a8405263ea774266ec46f5518cbd975cbd1
noahvillasana <noah.fv0@outlook.com>
2024-02-12 22:27:28 -0600

    Añadir eventos en click para abrir menu y estilos

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
 //MENU RESPONSIVE
 var btnMenuOpen = document.getElementById("btnMenuOpen"),
-btnMenuClose = document.getElementById("btnMenuClose"),
-menuResponsive = document.getElementById("menuBar"),
-enlaces = document.getElementById("enlaces");
+    btnMenuClose = document.getElementById("btnMenuClose"),
+    menuResponsive = document.getElementById("menuBar"),
+    enlaces = document.getElementById("enlaces");
 
 //CLICK ABRIR
-btnMenuOpen.addEventListener("click", function (){
-menuResponsive.classList.add("active");
+btnMenuOpen.addEventListener("click", function () {
+    menuResponsive.classList.add("active");
+    console.log("click abrir")
 });
 
 //CLICK CERRAR
-btnMenuClose.addEventListener("click", function (){
-menuResponsive.classList.remove("active");
+btnMenuClose.addEventListener("click", function () {
+    menuResponsive.classList.remove("active");
+    console.log("click cerrar")
 });
 
 //CERRAR MENU CON ELEMENTOS DE ENLACE
-enlaces.addEventListener("click", function(){
-menuResponsive.style.transitionDelay = "0.5s";
-menuResponsive.classList.remove("active");
+enlaces.addEventListener("click", function () {
+    menuResponsive.style.transitionDelay = "0.5s";
+    menuResponsive.classList.remove("active");
 });
+
+// shift + option + F
+// indenta el codigo
