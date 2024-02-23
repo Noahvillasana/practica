4319896c10c5a7afbac80b395d71288d8b012a61
noahvillasana <noah.fv0@outlook.com>
2024-02-23 01:01:57 -0600

    cambios con el slider

--- app.js
+++ app.js
@@ -24,3 +24,16 @@ enlaces.addEventListener("click", function () {
 
 // shift + option + F
 // indenta el codigo
+
+//SLIDER DE PRODUCTOS
+var contenedor = document.querySelector('.slider'),
+btnIzquierdo = document.getElementById("btn-izquierda"),
+btnDerecho = document.getElementById("btn-derecha");
+
+//EVENTO PARA EL BOTON DERECHO
+btnDerecho.addEventListener("click", function(){
+contenedor.scrollLeft += contenedor.offsetWidth;
+});
+btnIzquierdo.addEventListener("click", function(){
+    contenedor.scrollLeft -= contenedor.offsetWidth;
+    });
